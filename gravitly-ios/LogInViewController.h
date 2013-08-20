29c938840930614faaec0177d4139ec1d303d4b2
Geric Encarnacion <grencarnacion@dynamicobjx.com>
2013-08-20 12:14:12 +0800

    added log in view controller

---
+++ gravitly-ios/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  gravitly-ios
+//
+//  Created by Geric Encarnacion on 8/20/13.
+//  Copyright (c) 2013 Geric Encarnacion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
