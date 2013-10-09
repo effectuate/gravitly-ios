87b2638948390275f53e2d8fa5aa666ee7395b7d
Geric Encarnacion <grencarnacion@dynamicobjx.com>
2013-10-09 14:37:21 +0800

    doing get aws image + parse

---
+++ gravitly-ios/Controllers/PhotoFeedViewController.m
@@ -0,0 +1,110 @@
+//
+//  PhotoFeedViewController.m
+//  gravitly-ios
+//
+//  Created by Geric Encarnacion on 10/9/13.
+//  Copyright (c) 2013 Geric Encarnacion. All rights reserved.
+//
+
+#import "PhotoFeedViewController.h"
+#import <Parse/Parse.h>
+#import <AFNetworking.h>
+
+//define tags and xib here
+#define PHOTO_CELL @"photofeed"
+#define photo_tag 1
+
+@interface PhotoFeedViewController ()
+
+@end
+
+@implementation PhotoFeedViewController
+
+@synthesize photoTableView;
+@synthesize photos;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    //load cell here
+    [self.photoTableView registerNib:[UINib nibWithNibName:PHOTO_CELL bundle:nil] forHeaderFooterViewReuseIdentifier:PHOTO_CELL];
+    //get parse..
+    PFQuery *query = [PFQuery queryWithClassName:@"Photo"];
+    //[query orderByDescending:@"createdAt"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            NSLog(@"photssss");
+            self.photos = objects;
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+                        NSLog(@"photssss 2222");
+            [self.photoTableView reloadData];
+            //[self removeProgressHUD:self.tableView];
+        });
+    }];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+//====== tableview controller.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSLog(@"countss >>> %i", self.photos.count);
+    return self.photos.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [self.photoTableView dequeueReusableCellWithIdentifier:PHOTO_CELL];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:PHOTO_CELL];
+    }
+    
+    PFObject *element = [photos objectAtIndex:indexPath.row];
+    UIImageView *photoImage = (UIImageView *)[cell viewWithTag:photo_tag];
+    
+    NSString *imagepath = [NSString stringWithFormat:@"http://s3.amazonaws.com/gravitly.uploads.dev/%@",
+                           [element objectForKey:@"filename"]];
+    
+    AFHTTPClient *client = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:imagepath]];
+    [client getPath:imagepath parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSData *imageData = [UIImage imageNamed:imagepath];
+        UIImage *pugs = [UIImage imageWithData:imageData];
+        photoImage.image = pugs;
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"error");
+    }];
+    
+    NSLog(@"image path >> %@" ,imagepath);
+
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 170;
+}
+
+//=======
+
+@end
