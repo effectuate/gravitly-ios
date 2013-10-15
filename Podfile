8050dbb86ff41e95f1a21a970f848478e056543b
Giancarlo Inductivo <gian@dynamicobjx.com>
2013-10-15 20:48:56 +0800

    updated ignore list, retrieving bfcropinterface from cocoapods, added build incrementer phase, changed app name to 'Gravitly'

--- Podfile
+++ Podfile
@@ -4,3 +4,5 @@ pod 'TestFlightSDK', '~> 1.3.0.beta2'
 pod 'AFNetworking', '~> 1.3.2'
 pod 'MBProgressHUD', '~> 0.5'
 pod 'MLPAutoCompleteTextField', '~> 1.5'
+pod 'BFCropInterface', '~> 1.0.0'
+
--- Pods/Pods-header.h
+++
@@ -1,8 +0,0 @@
-// WARNING: This feature of CocoaPods is present for discussion purposes and might be discontinued or changed in future
-#define __COCOA_PODS
-
-#define __POD_AFNetworking
-#define __POD_GPUImage
-#define __POD_MBProgressHUD
-#define __POD_MLPAutoCompleteTextField
-#define __POD_TestFlightSDK
